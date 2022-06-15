1cf668f06d7d06d700d135420dc6f8d94c0368f6
Brian Marquis <rbmarquis.dev@gmail.com>
2022-06-14 20:01:05 -0500

    add input for fetching

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [url, setUrl] = useState(
+    'https://hn.algolia.com/api/v1/search?query=redux'
+  );
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        'https://hn.algolia.com/api/v1/search?query=redux'
-      );
+      const response = await fetch(url);
       const result = await response.json();
       setData(result);
     };
 
     fetchData();
-  }, []);
+  }, [url]);
 
   return (
     <div className="App">
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
       <ul>
         {data.hits.map(item => (
           <li key={item.objectID}>
