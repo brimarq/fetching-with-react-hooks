7b314dba51c0e53b9e45a18b47314c0211729ece
Brian Marquis <rbmarquis.dev@gmail.com>
2022-06-14 22:29:46 -0500

    add useReducer hook to consolidate state

--- src/hooks/useDataApi.js
+++ src/hooks/useDataApi.js
@@ -1,31 +1,55 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
 
 const useDataApi = (initialUrl, initialData) => {
-  const [data, setData] = useState(initialData);
   const [url, setUrl] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-
+      dispatch({ type: 'FETCH_INIT' });
       try {
         const response = await fetch(url);
         const result = await response.json();
-        setData(result);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result });
       } catch (error) {
-        setIsError(true);
+        dispatch({ type: 'FETCH_FAILURE' });
       }
-
-      setIsLoading(false);
     };
 
     fetchData();
   }, [url]);
 
-  return [{ data, isLoading, isError }, setUrl];
+  return [state, setUrl];
 };
 
 export default useDataApi;
